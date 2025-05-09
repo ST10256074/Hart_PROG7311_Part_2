5f986d84e8980bfd8f75bfb06957067b97c8bafe
James Warden Edgecombe Hart <ST10256074@vcconnect.edu.za>
2025-05-09 08:20:47 +0200

    Farmer CRUD

---
+++ Controllers/FarmerController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hart_PROG7311_Part_2.Data;
+using Hart_PROG7311_Part_2.Models;
+
+namespace Hart_PROG7311_Part_2.Controllers
+{
+    public class FarmerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FarmerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Farmer
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Farmers.ToListAsync());
+        }
+
+        // GET: Farmer/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmerModel = await _context.Farmers
+                .FirstOrDefaultAsync(m => m.FarmerModelID == id);
+            if (farmerModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmerModel);
+        }
+
+        // GET: Farmer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Farmer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FarmerModelID,Name,Username,Password,Address,ProfilePicture,PhoneNumber,CreateedAt")] FarmerModel farmerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(farmerModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(farmerModel);
+        }
+
+        // GET: Farmer/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmerModel = await _context.Farmers.FindAsync(id);
+            if (farmerModel == null)
+            {
+                return NotFound();
+            }
+            return View(farmerModel);
+        }
+
+        // POST: Farmer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FarmerModelID,Name,Username,Password,Address,ProfilePicture,PhoneNumber,CreateedAt")] FarmerModel farmerModel)
+        {
+            if (id != farmerModel.FarmerModelID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(farmerModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FarmerModelExists(farmerModel.FarmerModelID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(farmerModel);
+        }
+
+        // GET: Farmer/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmerModel = await _context.Farmers
+                .FirstOrDefaultAsync(m => m.FarmerModelID == id);
+            if (farmerModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmerModel);
+        }
+
+        // POST: Farmer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var farmerModel = await _context.Farmers.FindAsync(id);
+            if (farmerModel != null)
+            {
+                _context.Farmers.Remove(farmerModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FarmerModelExists(int id)
+        {
+            return _context.Farmers.Any(e => e.FarmerModelID == id);
+        }
+    }
+}
